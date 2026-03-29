1e4517d14b479e72b37460301dce0222137edf76
tanzz17 <tanavi1704@gmail.com>
2026-03-29 20:52:56 +0530

    Fix MySQL ID generation for Render startup

--- CloudBite-Cloud-Kitchen-Food-Delivery-Platform/src/main/java/com/cloudbite/model/Cart.java
+++ CloudBite-Cloud-Kitchen-Food-Delivery-Platform/src/main/java/com/cloudbite/model/Cart.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class Cart {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @ToString.Include
     private Long id;
 
--- CloudBite-Cloud-Kitchen-Food-Delivery-Platform/src/main/java/com/cloudbite/model/User.java
+++ CloudBite-Cloud-Kitchen-Food-Delivery-Platform/src/main/java/com/cloudbite/model/User.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @ToString.Include
     private Long id;
 
